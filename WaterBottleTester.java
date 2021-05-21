e407887e4d59f7a061abe010e7b8adef6feebfdc
Harshitha1308 <harshithak794@gmail.com>
2021-05-21 16:34:31 +0530

    add WaterBottleTester

---
+++ WaterBottleTester.java
@@ -0,0 +1,24 @@
+class WaterBottleTester{
+
+public static void main(String[] xyz){
+
+WaterBottle.displayAll();
+String brand="cello H20";
+WaterBottle.changeBrand(brand);
+int no=40;
+WaterBottle.changeQuantity(no);
+boolean thermoFlask=true;
+WaterBottle.changethermoFlask(thermoFlask);
+WaterBottle.displayAll();
+}
+}
+
+
+
+
+
+
+
+
+
+
