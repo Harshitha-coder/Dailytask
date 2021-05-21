46ebc624b98e6f0db108e8e4901d7c1a470c4647
Harshitha1308 <harshithak794@gmail.com>
2021-05-21 16:26:27 +0530

    add RefrigeratorTester

---
+++ RefrigeratorTester.java
@@ -0,0 +1,22 @@
+class RefrigeratorTester{
+
+public static void main(String[] xyz){
+
+Refrigerator.capacity=120;
+Refrigerator.displayopened();
+Refrigerator.open();
+Refrigerator.displayopened();
+Refrigerator.close();
+Refrigerator.displayopened();
+}
+}
+
+
+
+
+
+
+
+
+
+
