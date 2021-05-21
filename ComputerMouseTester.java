b9a19bca07830e46f43a0e1b6ee5f677f3d9a50d
Harshitha1308 <harshithak794@gmail.com>
2021-05-21 16:31:12 +0530

    add ComputerMouseTester

---
+++ ComputerMouseTester.java
@@ -0,0 +1,24 @@
+class ComputerMouseTester{
+
+public static void main(String[] xyz){
+ComputerMouse.displayAll();
+String wireless="wired";
+ComputerMouse.changeWireless(wireless);
+boolean chargeable=true;
+ComputerMouse.changeChargeable(chargeable);
+int z=60;
+ComputerMouse.changeRange(z);
+ComputerMouse.displayAll();
+
+}
+}
+
+
+
+
+
+
+
+
+
+
