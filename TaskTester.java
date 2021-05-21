2eebf35644740a25ed104f8cf894f0c8e67f21d0
Harshitha1308 <harshithak794@gmail.com>
2021-05-21 16:17:49 +0530

    add TaskTester

---
+++ TaskTester.java
@@ -0,0 +1,21 @@
+class TaskTester{
+
+public static void main(String[] veg){
+
+double range=Bulb.light();
+System.out.println(range);
+
+String name=Carpenter.woodWorks();
+System.out.println(name);
+
+String product=DeliveryGuy.delivery();
+System.out.println(product);
+
+String value=Farmer.farming();
+System.out.println(value);
+
+int time=Gym.openTime();
+System.out.println(time);
+
+}
+}
