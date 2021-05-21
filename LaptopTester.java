1b66f76080fc3c604d457be9c81913e491445423
Harshitha1308 <harshithak794@gmail.com>
2021-05-21 16:23:21 +0530

    add LaptopTester

---
+++ LaptopTester.java
@@ -0,0 +1,21 @@
+class LaptopTester{
+
+public static void main(String[] xyz){
+
+Laptop.display();
+Laptop.changeMemory();
+Laptop.display();
+Laptop.changeBrand();
+Laptop.display();
+}
+}
+
+
+
+
+
+
+
+
+
+
