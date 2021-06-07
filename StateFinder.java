a80e2100bfc4b5c3c9449428a454bc67d0b610f0
Harshitha1308 <harshithak794@gmail.com>
2021-06-07 18:35:09 +0530

    add StateFinder

---
+++ StateFinder.java
@@ -0,0 +1,211 @@
+class StateFinder{
+
+static String getStateName(String cityName){
+System.out.println("invoked getStateName");
+System.out.println(cityName);
+String stateName="NA";
+
+if(cityName.equals("Hyderabad")){
+String state="AndraPradesh";
+ stateName=state;
+System.out.println("stateName: " + stateName);
+return stateName;
+}
+
+if(cityName.equals("Itanagar")){
+String state="ArunachalPradesh";
+  stateName=state;
+System.out.println("stateName: " + stateName);
+return stateName;
+}
+
+if(cityName.equals("Dispur")){
+String state="Assam";
+  stateName=state;
+System.out.println("stateName: " + stateName);
+return stateName;
+}
+
+if(cityName.equals("Patna")){
+String state="Bihar";
+  stateName=state;
+System.out.println("stateName: " + stateName);
+return stateName;
+}
+
+if(cityName.equals("Raipur")){
+String state="Chattisgarh";
+  stateName=state;
+System.out.println("stateName: " + stateName);
+return stateName;
+}
+
+if(cityName.equals("Panaji")){
+String state="Goa";
+  stateName=state;
+System.out.println("stateName: " + stateName);
+return stateName;
+}
+
+if(cityName.equals("Gandhinagar")){
+String state="Gujarat";
+ stateName=state;
+System.out.println("stateName: " + stateName);
+return stateName;
+}
+
+if(cityName.equals("Chandigarh")){
+String state="Hariyana";
+  stateName=state;
+System.out.println("stateName: " + stateName);
+return stateName;
+}
+
+if(cityName.equals("Shimla")){
+String state="HimachalPradesh";
+  stateName=state;
+System.out.println("stateName: " + stateName);
+return stateName;
+}
+
+if(cityName.equals("Ranchi")){
+String state="Jharkhand";
+  stateName=state;
+System.out.println("stateName: " + stateName);
+return stateName;
+}
+
+if(cityName.equals("Banglore")){
+String state="Karnataka";
+ stateName=state;
+System.out.println("stateName: " + stateName);
+return stateName;
+}
+
+if(cityName.equals("Thiruvananthpuram")){
+String state="Kerala";
+ stateName=state;
+System.out.println("stateName: " + stateName);
+return stateName;
+}
+
+if(cityName.equals("Bhopal")){
+String state="MadhyaPradesh";
+ stateName=state;
+System.out.println("stateName: " + stateName);
+return stateName;
+}
+
+if(cityName.equals("Mumbai")){
+String state="Maharashtra";
+ stateName=state;
+System.out.println("stateName: " + stateName);
+return stateName;
+}
+
+if(cityName.equals("Imphal")){
+String state="Manipur";
+  stateName=state;
+System.out.println("stateName: " + stateName);
+return stateName;
+}
+
+if(cityName.equals("Shilong")){
+String state="Meghalaya";
+  stateName=state;
+System.out.println("stateName: " + stateName);
+return stateName;
+}
+
+if(cityName.equals("Aizawl")){
+String state="Mizoram";
+  stateName=state;
+System.out.println("stateName: " + stateName);
+return stateName;
+}
+
+if(cityName.equals("Kohima")){
+String state="Nagaland";
+  stateName=state;
+System.out.println("stateName: " + stateName);
+return stateName;
+}
+
+if(cityName.equals("Bhubaneswar")){
+String state="Odisha";
+ stateName=state;
+System.out.println("stateName: " + stateName);
+return stateName;
+}
+
+if(cityName.equals("Chandigarh")){
+String state="Punjab";
+  stateName=state;
+System.out.println("stateName: " + stateName);
+return stateName;
+}
+
+
+if(cityName.equals("Jaipur")){
+String state="Rajasthan";
+  stateName=state;
+System.out.println("stateName: " + stateName);
+return stateName;
+}
+
+if(cityName.equals("Gangtok")){
+String state="Sikkim";
+  stateName=state;
+System.out.println("stateName: " + stateName);
+return stateName;
+}
+
+if(cityName.equals("Chennai")){
+String state="TamilNadu";
+ stateName=state;
+System.out.println("stateName: " + stateName);
+return stateName;
+}
+
+if(cityName.equals("Hyderabad")){
+String state="Telegana";
+  stateName=state;
+System.out.println("stateName: " + stateName);
+return stateName;
+}
+
+if(cityName.equals("Agartala")){
+String state="Tripura";
+  stateName=state;
+System.out.println("stateName: " + stateName);
+return stateName;
+}
+
+if(cityName.equals("Lucknow")){
+String state="UttarPradesh";
+ stateName=state;
+System.out.println("stateName: " + stateName);
+return stateName;
+}
+
+if(cityName.equals("Dehradun")){
+String state="Uttarakhand";
+  stateName=state;
+System.out.println("stateName: " + stateName);
+return stateName;
+}
+
+if(cityName.equals("Kolkata")){
+String state="WestBengal";
+  stateName=state;
+System.out.println("stateName: " + stateName);
+return stateName;
+}
+
+
+
+System.out.println("getStateName End");
+return stateName;
+}
+
+}
