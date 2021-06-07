21df76ff586d95daa0493a003509ab44d3829551
Harshitha1308 <harshithak794@gmail.com>
2021-06-07 18:33:32 +0530

    add NameTester

---
+++ NameTester.java
@@ -0,0 +1,11 @@
+class NameTester{
+public static void main(String[] mon){
+	
+String name1="Sunil";
+
+ String fullname1=NameMatcher.getFullName(name1);
+ 
+ System.out.println(fullname1);
+ 
+}
+}
