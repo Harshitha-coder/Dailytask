a9bb448584d4a7e807c603febc579554025c9014
Harshitha1308 <harshithak794@gmail.com>
2021-06-07 18:29:56 +0530

    add MobileNumberTetser

---
+++ MobileNumberTester.java
@@ -0,0 +1,9 @@
+class MobileNumberTester{
+public static void main(String[] mon){
+	
+String name1="Harshitha";
+long mobilenumber1=MobileNumberFinder.getMobileNumber(name1);
+ System.out.println( mobilenumber1);
+ 
+ }
+ }
