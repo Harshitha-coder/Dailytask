615db58049592f4a126eedde8c050eb65f7a6c4e
Harshitha1308 <harshithak794@gmail.com>
2021-06-07 18:28:06 +0530

    add MobileNumberFinder

---
+++ MobileNumberFinder.java
@@ -0,0 +1,106 @@
+class MobileNumberFinder{
+
+static long getMobileNumber(String name){
+System.out.println("invoked getMobileNumber");
+System.out.println(name);
+long mobileNumber=00;
+
+if(name.equals("Harshitha")){
+long number=7899186099l;
+ mobileNumber=number;
+System.out.println("mobileNumber: " + mobileNumber);
+return mobileNumber;
+}
+if(name.equals("Aishwarya")){
+long number=8073528071l;
+ mobileNumber=number;
+System.out.println("mobileNumber: " + mobileNumber);
+return mobileNumber;
+}
+if(name.equals("Mradula")){
+long number=9611751258l;
+ mobileNumber=number;
+System.out.println("mobileNumber: " + mobileNumber);
+return mobileNumber;
+}
+if(name.equals("Sunita")){
+long number=8050811032l;
+ mobileNumber=number;
+System.out.println("mobileNumber: " + mobileNumber);
+return mobileNumber;
+}
+if(name.equals("Pavitra")){
+long number=7975157812l;
+ mobileNumber=number;
+System.out.println("mobileNumber: " + mobileNumber);
+return mobileNumber;
+}
+if(name.equals("Dharshan")){
+long number=7996657688l;
+ mobileNumber=number;
+System.out.println("mobileNumber: " + mobileNumber);
+return mobileNumber;
+}
+if(name.equals("Ranjita")){
+long number=8073721260l;
+ mobileNumber=number;
+System.out.println("mobileNumber: " + mobileNumber);
+return mobileNumber;
+}
+
+if(name.equals("Sachin")){
+long number=8095946885l;
+ mobileNumber=number;
+System.out.println("mobileNumber: " + mobileNumber);
+return mobileNumber;
+}
+if(name.equals("Mahesh")){
+long number=8618564838l;
+ mobileNumber=number;
+System.out.println("mobileNumber: " + mobileNumber);
+return mobileNumber;
+}
+if(name.equals("Sunil")){
+long number=8861484849l;
+ mobileNumber=number;
+System.out.println("mobileNumber: " + mobileNumber);
+return mobileNumber;
+}
+if(name.equals("Vaishnavi")){
+long number=9632802330l;
+ mobileNumber=number;
+System.out.println("mobileNumber: " + mobileNumber);
+return mobileNumber;
+}
+if(name.equals("Girija")){
+long number=9945413703l;
+ mobileNumber=number;
+System.out.println("mobileNumber: " + mobileNumber);
+return mobileNumber;
+}
+if(name.equals("Asha")){
+long number=9902408577l;
+ mobileNumber=number;
+System.out.println("mobileNumber: " + mobileNumber);
+return mobileNumber;
+}
+if(name.equals("Nikunj")){
+long number=9737366556l;
+ mobileNumber=number;
+System.out.println("mobileNumber: " + mobileNumber);
+return mobileNumber;
+}
+if(name.equals("Omkar")){
+long number=9986667354l;
+ mobileNumber=number;
+System.out.println("mobileNumber: " + mobileNumber);
+return mobileNumber;
+}
+
+System.out.println("getMobileNumber End");
+return mobileNumber;
+}
+}
+
+
+
