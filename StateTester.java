b3609c24cc8f174eba6529730649414380e5a31d
Harshitha1308 <harshithak794@gmail.com>
2021-06-07 18:36:36 +0530

    add StateTester

---
+++ StateTester.java
@@ -0,0 +1,11 @@
+class StateTester{
+public static void main(String[] mon){
+	
+String name1="Hyderabad";
+
+ String statename1=StateFinder.getStateName(name1);
+ 
+ System.out.println(statename1);
+ 
+}
+}
