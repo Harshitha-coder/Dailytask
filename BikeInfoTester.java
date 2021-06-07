f11ca564ac05d24e00cd397de82b899b7c7ab3a7
Harshitha1308 <harshithak794@gmail.com>
2021-06-07 18:06:10 +0530

    add BikeInfoTester

---
+++ BikeInfoTester.java
@@ -0,0 +1,11 @@
+class BikeInfoTester{
+
+public static void main(String[] num){
+ BikeInfo.displayAll();
+String brand="Royalenfield";
+long price=214673;
+int mileage= 37;
+ BikeInfo.setInfo(brand,price,Model.Royalenfield,Cc.cc346,mileage);
+ BikeInfo.displayAll();
+}
+}
