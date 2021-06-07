a4d59d791d46664f7a3e36d07ff69e29f2e42d7e
Harshitha1308 <harshithak794@gmail.com>
2021-06-07 18:56:47 +0530

    add ApllicationTester

---
+++ ApplicationTester.java
@@ -0,0 +1,19 @@
+class ApplicationTester{
+
+public static void main(String[] arr){
+Application.displayApplications();
+Application.addApplication("Whatsapp");
+Application.addApplication("Google Meet");
+Application.addApplication("Instagram");
+Application.addApplication("Amazon");
+Application.addApplication("Youtube");
+Application.addApplication("Telegram");
+Application.addApplication("Myntra");
+Application.addApplication("Snapchart");
+Application.addApplication("Google Drive");
+Application.addApplication("Gmail");
+Application.displayApplicationAtIndex(99);
+Application.printFoundIfApplication("Amazon");
+}
+
+}
