88330449f568eb6bfd8e1467c8f8bf2fe085214d
Harshitha1308 <harshithak794@gmail.com>
2021-06-11 17:47:57 +0530

    add ChoclateArrayTester

---
+++ ChoclateArrayTester.java
@@ -0,0 +1,19 @@
+class ChoclateArrayTester{
+ 
+ public static void main(String[] choc){
+  String choclate1="KitKat";
+  String choclate2="DairyMilk";
+  String choclate3="Snickers";
+  String choclate4="Hersheys";
+  String choclate5="Nestle";
+  String choclate6="FiveStar";
+   String[] choclate={choclate1,choclate2,choclate3,choclate4,choclate5,choclate6};
+  System.out.println("choclate.length "+ choclate.length);
+     System.out.println(choclate[0]);
+	  System.out.println(choclate[1]);
+	   System.out.println(choclate[2]);
+	    System.out.println(choclate[3]);
+		 System.out.println(choclate[4]);
+		  System.out.println(choclate[5]);
+ }
+}
